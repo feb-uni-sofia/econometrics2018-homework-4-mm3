076d9c87d3b95b49ac2cd6b3276f446aaa646d8c
mihaellaiv <mihaela.iv.ivanova@gmail.com>
2018-06-15 16:55:37 +0300

    homework4

--- Problem_1.R
+++ Problem_1.R
@@ -7,21 +7,43 @@ houseWork <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/da
 str(houseWork)
 
 ## a)
+summary(houseWork$sex)
 
 ## b)
+muf <- mean(houseWork$hours[houseWork$sex == 'f'])
+mum <- mean(houseWork$hours[houseWork$sex == 'm'])
+
 
 ## c)
+houseWork$female <- ifelse(houseWork$sex == 'f', 'TRUE', 'FALSE')
+houseWork$male <- ifelse(houseWork$sex == 'm', 'TRUE', 'FALSE')
+
 
 ## d)
+fit <- lm(hours ~ female, data = houseWork)
+summary(fit)
 
-## e)
 
-## f)
+## e)It is a positive intercept that shows: if women are not involved in housework the work that will be done by all men is the average value of work hours by men per week
+## The other coefficient is negative, which means that women overall work less on average so if we add another woman it would decrease the overall average working hours per week.
+
+## f) the null hypothesis: H0 beta1 >= 0 then the alternative hypothesis is: H1 beta1 < 0
+## The null hypothesis states that women's work hours contribute to the average population work hours and the alternative hypothesis states the opposite
+## From the results of the linear model, we can reject the null hypothesis.
 
 ## g)
+populationMean <- mean(houseWork$hours)
+testStatistic <- sqrt(11016)*(muf - populationMean) / 0.3186
+pt(testStatistic, df = 11014)
 
-## h)
 
-## i)
+## h) There is zero probability to make a mistake when rejecting the null hypothesis if it is true, so we can reject the null hypothesis.
+
+## i) The population follows a t-distribution. The expectations are that the distribution is normal, because we are estimating time values
 
 ## j)
+fit1 <- lm(hours ~ female + male, data = houseWork)
+summary(fit1)
+
+## Y = beta0 + beta1*X1 + beta2*X2
+
--- Problem_2.R
+++ Problem_2.R
@@ -2,12 +2,26 @@
 crime <- read.delim('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/crime.csv', stringsAsFactors = FALSE)
 str(crime)
 
-## a)
+library(dplyr)
+library(ggplot2)
+
+## a) crime rates increase with the increase in high-school graduates percentage.
+fit <- lm(C ~ HS, data = crime)
+summary(fit)
+
 
 ## b)
+pairs( ~ C + U + I + HS, data = crime)
 
-## c)
+## c) There is multicolinearity across feature variables which changes the estimates substantially. That means that one feature variable can be explaine by another feature variable, thus the estimates may not reproduce valid estimates for the change in the explained variable
 
-## d)
+fit1 <- lm(C ~ HS + U, data = crime)
+summary(fit1)
+
+## d) This may be true but we should note that the multicolinearity may affect the estimated coefficient values
+## There are other variables which influence the crime rate but are omitted in our model.
 
 ## e)
+fit2 <- lm(C ~ HS + U + I, data = crime)
+summary(fit2)
+
